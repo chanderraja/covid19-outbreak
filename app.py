5e2550909f3977597c46e02fde1ea712b7511286
Chander Raja <chander.raja@gmail.com>
2020-03-21 01:54:15 -0400

    map with hovertext

---
+++ app.py
@@ -0,0 +1,111 @@
+import os
+import plotly.graph_objects as go
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
+import pandas as pd
+import io
+import requests
+
+
+url='https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-20-2020.csv'
+s=requests.get(url).content
+df=pd.read_csv(io.StringIO(s.decode('utf-8')))
+
+def get_location(row):
+    state = row['Province/State']
+    country = row['Country/Region']
+    if pd.isna(state):
+        return country
+    return state + '<br>' + country
+
+def get_hovertext(row):
+    return get_location(row) + '<br>' + 'Confirmed = ' + str(row['Confirmed']) + \
+           '<br>' + 'Deaths = ' + str(row['Deaths']) + '<br>' + 'Recovered =' + str(row['Recovered'])
+
+df['hovertext'] = df.apply(lambda row: get_hovertext(row), axis=1)
+
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+mapbox_access_token = os.environ.get('MAPBOX_TOKEN')
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+
+def get_mapbox():
+    datamap =  [dict(
+        type='scattermapbox',
+        lat=df['Latitude'],
+        lon=df['Longitude'],
+        mode='markers',
+        marker_size=df['Confirmed'],
+        text=df['hovertext']
+    )]
+    layout = dict(
+        autosize=True,
+        height=600,
+        hovermode='closest',
+        geo = dict(
+            projection = dict(
+                type = 'equirectangular'
+            ),
+        ),
+        mapbox=dict(
+            mapbox_style='dark',
+            accesstoken=mapbox_access_token,
+            bearing=0,
+            center=dict(
+                lat=38.92,
+                lon=-77.07
+            ),
+            pitch=0,
+            zoom=2
+        ),
+    )
+
+    fig = dict(
+        data=datamap,
+        layout=layout
+    )
+
+    return dcc.Graph(
+        id='id-mapbox',
+        figure=fig
+    )
+
+
+def serve_layout():
+    return html.Div(
+        [
+            html.H4(
+                'COVID-19 Outbreak Dashboard',
+                style={
+                    'color': 'blue',
+                    'font-style': 'italic',
+                    'font-weight': 'bold',
+                    'height': '50px',
+                    'display': 'inline-block'
+                }
+            ),
+            dcc.Loading(
+                id='id-loading',
+                children=[
+                    html.Div(
+                        [
+                            get_mapbox()
+                        ],
+                        id='id-map-container'
+                    ),
+                ],
+                type='circle'
+            ),
+        ]
+    )
+
+
+app.layout = serve_layout()
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
