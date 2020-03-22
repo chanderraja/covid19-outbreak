69d6d46c9578a13d3aaf3cd928c4ff78886d354a
Chander Raja <chander.raja@gmail.com>
2020-03-22 19:47:28 -0400

    Work in progress for location input

--- app.py
+++ app.py
@@ -9,14 +9,24 @@ import pandas as pd
 import io
 import requests
 
+COL_PROVINCE_STATE='Province/State'
+COL_STATE='State'
+COL_COUNTRY_REGION='Country/Region'
+COL_COUNTRY='Country'
+COL_LATITUDE='Latitude'
+COL_LONGITUDE='Longitude'
+
 
 url='https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-21-2020.csv'
 s=requests.get(url).content
 df=pd.read_csv(io.StringIO(s.decode('utf-8')))
+# clean up column names
+df =  df.rename(columns={COL_PROVINCE_STATE: COL_STATE, COL_COUNTRY_REGION: COL_COUNTRY})
+
 
 def get_location(row):
-    state = row['Province/State']
-    country = row['Country/Region']
+    state = row[COL_STATE]
+    country = row[COL_COUNTRY]
     if pd.isna(state):
         return country
     return state + '<br>' + country
@@ -27,13 +37,40 @@ def get_hovertext(row):
 
 df['hovertext'] = df.apply(lambda row: get_hovertext(row), axis=1)
 
+
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 mapbox_access_token = os.environ.get('MAPBOX_TOKEN')
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
-def get_mapbox():
-    datamap =  [dict(
+def get_location_suggestions():
+    suggestions = df[COL_COUNTRY].unique().tolist()
+    suggestions += df[COL_STATE].fillna('').unique().tolist()
+    suggestions = sorted(suggestions)
+    return suggestions
+
+
+
+DEFAULT_LATITUDE=38.92
+DEFAULT_LONGITUDE=-77.07
+DEFAULT_ZOOM=2
+
+
+def get_latlong_and_zoom(location):
+    if location is not None and location is not '':
+        rows = df[df.Country == location]
+        if rows.shape[0] == 0:
+            rows = df[df.State == location]
+
+        if rows.shape[0] != 0:
+            row = rows.iloc[0]
+            return row[COL_LATITUDE], row[COL_LONGITUDE], 4
+    return DEFAULT_LATITUDE, DEFAULT_LONGITUDE, DEFAULT_ZOOM
+
+
+def get_mapbox(center_latitude, center_longitude, zoom):
+    datamap =  dict(
         type='scattermapbox',
         lat=df['Latitude'],
         lon=df['Longitude'],
@@ -45,10 +82,9 @@ def get_mapbox():
             color='rgb(180,0,0)',
         ),
         text=df['hovertext']
-    )]
+    )
     layout = dict(
         autosize=True,
-        height=600,
         hovermode='closest',
         geo = dict(
             projection = dict(
@@ -56,27 +92,23 @@ def get_mapbox():
             ),
         ),
         mapbox=dict(
-            style='dark',
+            style='light',
             accesstoken=mapbox_access_token,
             bearing=0,
             center=dict(
-                lat=38.92,
-                lon=-77.07
+                lat=center_latitude,
+                lon=center_longitude
             ),
             pitch=0,
-            zoom=2
+            zoom=zoom
         ),
     )
 
     fig = dict(
-        data=datamap,
+        data=[datamap],
         layout=layout
     )
-
-    return dcc.Graph(
-        id='id-mapbox',
-        figure=fig
-    )
+    return fig
 
 
 def serve_layout():
@@ -89,16 +121,29 @@ def serve_layout():
                     'font-style': 'italic',
                     'font-weight': 'bold',
                     'height': '50px',
-                    'display': 'inline-block'
+                    'display': 'block',
+                    'text-align': 'center',
+                    'left-margin': 'auto',
+                    'right-margin': 'auto'
                 }
             ),
+            html.Datalist(
+                id='id-list-suggested-inputs',
+                children=[html.Option(value=word) for word in get_location_suggestions()]
+            ),
+            dcc.Input(id='id-input-loc',
+                      type='text',
+                      list='id-list-suggested-inputs',
+                      value=''
+                      ),
             dcc.Loading(
                 id='id-loading',
                 children=[
                     html.Div(
-                        [
-                            get_mapbox()
-                        ],
+                        dcc.Graph(
+                            id='id-mapbox',
+                            figure={}
+                        ),
                         id='id-map-container'
                     ),
                 ],
@@ -109,6 +154,15 @@ def serve_layout():
 
 
 app.layout = serve_layout()
+
+@app.callback(
+    Output('id-mapbox', 'figure'),
+    [Input('id-input-loc', 'value')]
+)
+def location_selected(location):
+    lat, long, zoom = get_latlong_and_zoom(location)
+    app.logger.warning(f'location={location}, lat={lat}, long={long}, zoom={zoom}')
+    return get_mapbox(lat, long, zoom)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
