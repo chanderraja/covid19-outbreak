e5aa2f9b6ea74a4e175ff12299158be4ae56d3c4
Chander Raja <chander.raja@gmail.com>
2020-03-26 19:52:10 -0400

    Added USA choropleth map

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ from dash.exceptions import PreventUpdate
 import pandas as pd
 import io
 import requests
+import json
+from urllib.request import urlopen
+import numpy as np
 
+COL_FIPS='FIPS'
 COL_PROVINCE_STATE='Province_State'
 COL_COUNTRY_REGION='Country_Region'
 COL_ADMIN2='Admin2'
@@ -22,10 +26,13 @@ COL_LOC_COMBINED='Combined_Key'
 COL_HOVERTEXT='Hovertext'
 
 
-url='https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-25-2020.csv'
+url='https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/'+\
+    'csse_covid_19_data/csse_covid_19_daily_reports/03-25-2020.csv'
 s=requests.get(url).content
 df=pd.read_csv(io.StringIO(s.decode('utf-8')))
 
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
 
 def get_location(row):
     return row[COL_LOC_COMBINED]
@@ -39,7 +46,9 @@ def get_hovertext(row):
 
 df[COL_HOVERTEXT] = df.apply(lambda row: get_hovertext(row), axis=1)
 
-
+df_usa = df[df[COL_COUNTRY_REGION]=='US']
+df_usa = df[df[COL_FIPS].notna()] # drop rows with NaN in FIPS column
+df_usa[COL_CONFIRMED] = df_usa[COL_CONFIRMED].replace(0, .000001)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 mapbox_access_token = os.environ.get('MAPBOX_TOKEN')
@@ -53,7 +62,7 @@ def get_location_suggestions():
     suggestions = sorted(suggestions)
     return suggestions
 
-
+location_suggestions = get_location_suggestions()
 
 DEFAULT_LATITUDE=38.92
 DEFAULT_LONGITUDE=-77.07
@@ -62,16 +71,28 @@ DEFAULT_ZOOM=2
 
 def get_latlong_and_zoom(location):
     if location is not None and location is not '':
+        zoom = 4
         rows = df[df[COL_COUNTRY_REGION] == location]
         if rows.shape[0] == 0:
             rows = df[df[COL_PROVINCE_STATE] == location]
+            zoom=5
         if rows.shape[0] == 0:
             rows = df[df[COL_ADMIN2] == location]
+            zoom = 6
         if rows.shape[0] != 0:
             row = rows.iloc[0]
-            return row[COL_LATITUDE], row[COL_LONGITUDE], 4
+            return row[COL_LATITUDE], row[COL_LONGITUDE], zoom
     return DEFAULT_LATITUDE, DEFAULT_LONGITUDE, DEFAULT_ZOOM
 
+def get_choropleth_mapbox():
+    fig = go.Figure(go.Choroplethmapbox(geojson=counties, locations=df_usa.FIPS,
+                                        z=np.log10(df_usa.Confirmed),
+                                        colorscale="Viridis",
+                                        zmin=0, zmax=df_usa.Confirmed.max(), marker_line_width=0))
+    fig.update_layout(mapbox_style="light", mapbox_accesstoken=mapbox_access_token,
+                      mapbox_zoom=3, mapbox_center={"lat": 37.0902, "lon": -95.7129})
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    return fig
 
 def get_mapbox(center_latitude, center_longitude, zoom):
     datamap =  dict(
@@ -106,6 +127,7 @@ def get_mapbox(center_latitude, center_longitude, zoom):
             pitch=0,
             zoom=zoom
         ),
+        height=800,
     )
 
     fig = dict(
@@ -133,17 +155,25 @@ def serve_layout():
             ),
             html.Datalist(
                 id='id-list-suggested-inputs',
-                children=[html.Option(value=word) for word in get_location_suggestions()]
+                children=[html.Option(value=word) for word in location_suggestions]
             ),
-            dcc.Input(id='id-input-loc',
-                      type='text',
-                      list='id-list-suggested-inputs',
-                      value=''
-                      ),
-            dcc.Graph(
-                id='id-mapbox',
-                figure={}
-            )
+
+            dcc.Tabs([
+                dcc.Tab(label='World', children=[
+                    dcc.Input(id='id-input-loc',
+                              type='text',
+                              list='id-list-suggested-inputs',
+                              value=''),
+                    dcc.Graph(
+                        id='id-mapbox-world',
+                        figure={})
+                ]),
+                dcc.Tab(label='USA', children=[
+                    dcc.Graph(
+                        id='id-mapbox-usa',
+                        figure=get_choropleth_mapbox())
+                ]),
+            ]),
         ]
     )
 
@@ -151,7 +181,7 @@ def serve_layout():
 app.layout = serve_layout()
 
 @app.callback(
-    Output('id-mapbox', 'figure'),
+    Output('id-mapbox-world', 'figure'),
     [Input('id-input-loc', 'value')]
 )
 def location_selected(location):
